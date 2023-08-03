cf6694dc0d459b033e39295dbe61e417f95cedd7
Greyson Santos <greyson5309@icloud.com>
2023-08-03 10:36:34 -0400

    Changes

---
+++ Basic Data Types Activity/storytime.js
@@ -0,0 +1,22 @@
+// 4-Strings
+let greetings = "Hello ";
+let firstName = 'Greyson';
+let whereLive = 'Bel Air, Maryland';
+let favFood = 'burgers'; 
+
+// 3-Integers
+let age = 14;
+let siblings = 1;
+let year = 2023;
+
+// 1-Array
+let favThings = ['video games',' food',' sports']; 
+
+// 1-Booleans
+let hasDog = true;
+
+console.log(greetings + " My name is " + firstName + ".");
+console.log("I live in " + whereLive + ".");
+console.log("The year is " + year + " and I am " + age + " years old.");
+console.log("I have " + siblings + "sibling and it is " + hasDog + " that I have a dog.");
+console.log("My favortie things are " + favThings + ".");
